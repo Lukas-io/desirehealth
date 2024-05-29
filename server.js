3ecbe741079b32a502516fd115242df4c5062b3d
Lukas-io <120050725+Lukas-io@users.noreply.github.com>
2024-05-29 15:48:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post("/api/signin", async (req, res) => {
 });
 
 // Route for user sign-up
-app.get("/api/signup", async (req, res) => {
+app.post("/api/signup", async (req, res) => {
   try {
     const { email, password, userType, ...userData } = req.body;
 
